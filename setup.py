c2d30adfb7dcd945263aa0f731cf9c07645da092
Akihiro Kuno <akikuno@users.noreply.github.com>
2022-06-22 17:50:49 +0900

    create setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="wslPath",
+    version="0.2.0",
+    author="Akihiro Kuno",
+    author_email="akuno@md.tsukuba.ac.jp",
+    description="Python module to convert between Windows and POSIX path in WSL",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/akikuno/wslPath",
+    packages=setuptools.find_packages(where="src",),
+    package_dir={"": "src"},
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+)
